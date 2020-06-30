c5fa08fc69c131b40f43e21e876d4c0c25fe75ae
Yevhenii Rezanov <50371802+yevhenii-r@users.noreply.github.com>
2020-06-30 12:13:17 +0300

    feat: Update application (#3)

--- packages/app/index.js
+++ packages/app/index.js
@@ -1,7 +1,7 @@
 const first = require('@monorepo/first');
 const second = require('@monorepo/second');
 
-const app = () => 'Hi from the new app';
+const app = () => 'Hi from the new application';
 
 const main = () => {
   console.log(app());
