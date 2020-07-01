d7dbc44dad16bd25be043ba89afec7e4f793cf62
yevhenii-r <yevhenii@astrumu.com>
2020-07-01 17:25:02 +0300

    feat: Add Local Module

--- packages/local/test.js
+++ packages/local/test.js
@@ -2,7 +2,7 @@ const local = require('.');
 
 describe('local', () => {
   it('should return correct message', () => {
-    const result = firlocalst();
+    const result = local();
     expect(result).toBe('Hi from the local module');
   });
 });
